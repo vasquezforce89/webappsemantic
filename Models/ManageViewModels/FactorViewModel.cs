b0641ce29d47ea3e3ab142694ff26d90ebe78186
vasquezforce89 <vasquez.junior08@gmail.com>
2016-11-29 18:02:25 -0500

    initial commit

---
+++ Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webappsemantic.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
